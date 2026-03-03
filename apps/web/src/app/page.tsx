237b67c94e3d01bccdeaaaf1e661de4e77ed388b
Esteban Abanto <esteban.abanto.2709@gmail.com>
2026-03-02 21:04:32 -0500

    feat(web/Shadcn UI): add UI components and global styles for application

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
@@ -34,6 +35,9 @@ export default function Home() {
             center.
           </p>
         </div>
+        <div className="p-6">
+          <Input placeholder="Peso en kg" />
+        </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
@@ -51,7 +55,7 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
