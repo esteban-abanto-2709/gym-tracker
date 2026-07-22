54b4a0b1fedda9fc5c8fe08a693890da5bd8a96a
Esteban Abanto <esteban.abanto.2709@gmail.com>
2026-07-22 13:09:15 -0500

    feat(api): add google authentication support and update User model

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -37,7 +37,11 @@ export class AuthService {
     const user = await this.prisma.user.findUnique({
       where: { email: dto.email },
     });
-    if (!user || !(await bcrypt.compare(dto.password, user.passwordHash))) {
+    if (
+      !user ||
+      !user.passwordHash ||
+      !(await bcrypt.compare(dto.password, user.passwordHash))
+    ) {
       throw new UnauthorizedException('Invalid credentials');
     }
     return this.publicUser(user);
